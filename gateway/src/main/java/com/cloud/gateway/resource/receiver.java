daaf17cc6580ced9e4bee0d7a5d8847cb433f85a
manohajx <manoharj@tcd.ie>
2019-02-12 15:44:20 +0000

    Initial commit

---
+++ gateway/src/main/java/com/cloud/gateway/resource/receiver.java
@@ -0,0 +1,59 @@
+package com.cloud.gateway.resource;
+
+import com.cloud.gateway.BaseResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/gateway")
+public class receiver {
+
+    private final String sharedKey = "SHARED_KEY";
+
+    private static final String SUCCESS_STATUS = "success";
+
+    private static final String ERROR_STATUS = "error";
+
+    private static final int CODE_SUCCESS = 100;
+
+    private static final int AUTH_FAILURE = 102;
+
+    @RequestMapping("/sendHello")
+    public String sendHello(){
+
+        return ("Hello");
+    }
+
+
+    @RequestMapping(value = "/directions", method = RequestMethod.POST)
+    public Map<String, Object>  directions(@RequestParam(value = "key") String key, @RequestBody Map<String, Object> payLoad){
+        BaseResponse response = new BaseResponse();
+        if (sharedKey.equalsIgnoreCase(key)) {
+
+            response.setStatus(SUCCESS_STATUS);
+
+            response.setCode(CODE_SUCCESS);
+
+        } else {
+
+            response.setStatus(ERROR_STATUS);
+
+            response.setCode(AUTH_FAILURE);
+
+        }
+
+        return payLoad;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
